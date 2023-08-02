d884351c5289b651e405a3f3059594c4e7ccb468
Muhammed Shittu <devshittu@gmail.com>
2023-08-02 13:55:02 +0100

    Step 12 completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,25 @@
 import './App.css';
+import {SearchResults} from '../SearchResults/SearchResults'
+import { Playlist } from "../Playlist/Playlist";
+import { SearchBar } from "../SearchBar/SearchBar";
 
 function App() {
   return (
     <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    {/* <!-- Add a SearchBar component --> */}
-    <div className="App-playlist">
-      {/* <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component --> */}
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <SearchBar />
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component -->*/}
+          <SearchResults />
+          {/*<!-- Add a Playlist component --> */}
+          <Playlist />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 }
 
